9f25d77ee6ecdfe0ffdc23a22faac6cc622502c2
jongwoo Yoo <dbwhddn10@gmail.com>
2024-11-21 02:04:05 +0900

    fix wrong word 'server' -> 'service'

--- src/main/java/io/github/dbwhddn10/simplify_server_layer/Hello.java
+++
@@ -1,7 +0,0 @@
-package io.github.dbwhddn10.simplify_server_layer;
-
-public class Hello {
-    public static void world() {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/io/github/dbwhddn10/simplify_service_layer/Hello.java
@@ -0,0 +1,7 @@
+package io.github.dbwhddn10.simplify_service_layer;
+
+public class Hello {
+  public static void world() {
+    System.out.println("Hello world!");
+  }
+}
